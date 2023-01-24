072d2334b505e7c55f49e151087b739dd56fe767
Muthu <111842489+Muthumuthu007@users.noreply.github.com>
2023-01-24 13:47:05 +0530

    Create decimal cube and square.cpp

---
+++ decimal cube and square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,cube,square;
+   
+    cout<<"enter the decimal number:";
+    cin>>n;
+    cube=n*n*n;
+    square=n*n;
+    cout<<"\ncube number:"<<cube;
+    cout<<"\nsquare number:"<<square;
+}
